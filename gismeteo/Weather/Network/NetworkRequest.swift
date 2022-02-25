978790c97233ef26b9bfdbbc843f3f7a048558e1
Islam Kompaev <islamkompaev@mail.ru>
2022-02-25 17:11:13 +0300

    добавил коменты

--- gismeteo/Weather/Network/NetworkRequest.swift
+++ gismeteo/Weather/Network/NetworkRequest.swift
@@ -10,7 +10,7 @@ import Alamofire
 class  NetworkService {
     static let shared = NetworkService()
     public func getTheWeather(city: String) -> () {
-    let cityTypeURL = city.addingPercentEncoding(withAllowedCharacters: .urlHostAllowed)  ?? ""
+    let cityTypeURL = city.addingPercentEncoding(withAllowedCharacters: .urlHostAllowed)  ?? "" // дошел 
     let url = "https://api.openweathermap.org/data/2.5/weather?q=\(cityTypeURL)&units=metric&appid=4f99e23623c0c1dca52c1a64c1172c45&lang=ru"
 AF.request(url).responseDecodable(of: Weather.self) { (response) in
     
